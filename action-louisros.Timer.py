adf4cfd50600bce763525964497e29ec47d33397
kheperV3 <louis@ros.fr>
2018-06-20 18:24:50 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -5,6 +5,8 @@ import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import requests
+import json
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
@@ -29,11 +31,23 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 
 def action_wrapper(hermes, intentMessage, conf):
-    current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, "c'est fait Monsieur")
+ 
+
+v = intentMessage.slots.ville.first().value
+r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=v&appid=c427cad62f8bfde6ed0e800f59e1a39d&lang=fr&units=metric")
+data=r.json()
+
+s = data["weather"][0]
+d = s["description"]
+tm  = data["main"]["temp_min"]
+tM = data["main"]["temp_max"]
+
+m =u"Ce jour à " + ville + " " +  d + u" température entre " + str(tm) + " et " + str(tM) + u" degrés"
+  
+current_session_id = intentMessage.session_id
+hermes.publish_end_session(current_session_id, m)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("louisros:settimer", subscribe_intent_callback) \
-.start()
+        h.subscribe_intent("louisros:today", subscribe_intent_callback).start()
