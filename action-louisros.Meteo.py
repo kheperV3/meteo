c6b715a2b417a165647d0f93d2c62a2cae1d3fa4
kheperV3 <louis@ros.fr>
2018-06-20 19:34:17 +0200

    Update action-louisros.Meteo.py

--- action-louisros.Meteo.py
+++ action-louisros.Meteo.py
@@ -51,4 +51,5 @@ m =u"Ce jour à " + ville + " " +  d + u" température entre " + str(tm) + " et 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("louisros:today", subscribe_intent_callback).start()
+        h.subscribe_intent("louisros:today", subscribe_intent_callback) \
+        .start()
